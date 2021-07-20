bdf3d6cdbf1001bbea07595107f8291dc39c568e
Scuftz <shivneel.singh1600@gmail.com>
2021-07-21 00:46:00 +1200

    Set up sign up screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <>
+        <View style={styles.container}>
             <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
+                <Text h3>Sign Up for ToothMate</Text>
             </Spacer>
-            <Input label="Email" />
-            <Spacer />
-            <Input label="Password" />
+            <Spacer/>
+            <Input label="First Name" value={firstName} onChangeText={setFirstName} autoCapitalize="none" autoCorrect={false} />
+            <Input label="Last Name" value={lastName} onChangeText={setLastName} autoCapitalize="none" autoCorrect={false} />
+            <Input label="Email" value={email} onChangeText={setEmail} autoCapitalize="none" autoCorrect={false} />
+            <Input label="Mobile" value={mobile} onChangeText={setMobile} autoCapitalize="none" autoCorrect={false} />
+            <Input label="Password" value={password} onChangeText={setPassword} autoCapitalize="none" autoCorrect={false} secureTextEntry />
             <Spacer>
                 <Button title="Sign Up" />
             </Spacer>
-        </>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 30
+    }
+});
 
 export default SignupScreen;
