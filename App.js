da73c4e84289c908706e1a9a3268fed82ff1f39f
Scuftz <shivneel.singh1600@gmail.com>
2021-07-21 00:21:07 +1200

    Setting up navigation

--- App.js
+++ App.js
@@ -1,4 +1,19 @@
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    Account: AccountScreen
+  })
+});
+
+export default createAppContainer(switchNavigator);
