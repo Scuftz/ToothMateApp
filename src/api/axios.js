ad242ab896f6663483eae5915eaee08fcd89356d
royalty37 <patu_maori@hotmail.com>
2021-09-01 02:01:41 +1200

    Fixed navigation issue with add child account feature

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://0806-2407-7000-81ea-4200-da-6fcb-699c-82d3.ngrok.io",
+  baseURL: "https://f37b-121-98-12-136.ngrok.io",
 });
