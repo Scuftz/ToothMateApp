968bed41d7e4391c652af8a853c000eb6afab056
Scuftz <shivneel.singh1600@gmail.com>
2021-09-02 17:26:11 +1200

    sign in screen v1 with css

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://b0ef-2407-7000-81ea-4200-19f7-5399-69f4-9a3a.ngrok.io",
+  baseURL: "http://263a-2407-7000-81ea-4200-19f7-5399-69f4-9a3a.ngrok.io",
 });
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { NavigationEvents } from "react-navigation";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
@@ -14,19 +14,36 @@ const SigninScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <NavigationEvents onWillFocus={clearErrorMessage} />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../components/dental_background.jpg")}
+        />
+      </View>
       <Spacer>
-        <Text h3>Sign In</Text>
+        <Spacer />
+        <Text h3 style={styles.signinText}>
+          Sign In
+        </Text>
       </Spacer>
       <Spacer />
       <Input
-        label="Email"
+        // label="Email"
+        placeholder="Email"
+        leftIcon={{ type: "material-icons", name: "email" }}
+        inputContainerStyle={styles.inputContainer}
+        inputStyle={styles.inputStyle}
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
         autoCorrect={false}
       />
       <Input
-        label="Password"
+        // label="Password"
+        inputContainerStyle={styles.inputContainer}
+        leftIcon={{ type: "font-awesome", name: "lock" }}
+        inputStyle={styles.inputStyle}
+        placeholder="Password"
         value={password}
         onChangeText={setPassword}
         autoCapitalize="none"
@@ -36,14 +53,20 @@ const SigninScreen = ({ navigation }) => {
       {state.errorMessage ? (
         <Text style={styles.errorMessage}>{state.errorMessage}</Text>
       ) : null}
-      <Spacer>
-        <Button title="Sign In" onPress={() => signin({ email, password })} />
-      </Spacer>
+      <Button
+        buttonStyle={styles.button}
+        containerStyle={styles.buttonContainer}
+        // raised="true"
+        title="Sign In"
+        onPress={() => signin({ email, password })}
+      />
+      <Spacer />
       <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
         <Spacer>
-          <Text style={styles.link}>
-            Don't have an account? Sign up instead
-          </Text>
+          <View style={styles.link}>
+            <Text style={styles.linkText}>Don't have an account? </Text>
+            <Text style={styles.secondLinkText}>Sign up instead</Text>
+          </View>
         </Spacer>
       </TouchableOpacity>
     </View>
@@ -59,8 +82,44 @@ SigninScreen.navigationOptions = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    marginBottom: 200,
+    justifyContent: "flex-start",
+    // marginBottom: 200,
+  },
+  imageContainer: {
+    width: "100%",
+    height: 177,
+    marginBottom: 25,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "stretch",
+    borderColor: "blue",
+  },
+  buttonContainer: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "#21cbff",
+    width: "90%",
+    marginLeft: "5%",
+  },
+  button: {
+    paddingVertical: 10,
+    backgroundColor: "#21cbff",
+  },
+  inputStyle: {
+    paddingVertical: 8,
+    padding: 5,
+    fontSize: 16,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "#dedede",
+    width: "95%",
+    paddingLeft: 10,
+    backgroundColor: "#ebebeb",
+    marginLeft: "2.5%",
   },
   errorMessage: {
     fontSize: 16,
@@ -68,7 +127,20 @@ const styles = StyleSheet.create({
     marginLeft: 15,
   },
   link: {
-    color: "blue",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginRight: "5%",
+  },
+  linkText: {
+    color: "gray",
+    fontWeight: "bold",
+  },
+  secondLinkText: {
+    color: "#005a8a",
+    fontWeight: "bold",
+  },
+  signinText: {
+    marginLeft: "2.5%",
   },
 });
 
