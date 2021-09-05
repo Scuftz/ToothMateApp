84efc754eeec416299454bca5bf0920b26ad892f
Scuftz <shivneel.singh1600@gmail.com>
2021-09-05 14:37:04 +1200

    first sign in design

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://263a-2407-7000-81ea-4200-19f7-5399-69f4-9a3a.ngrok.io",
+  baseURL: "http://da5d-2407-7000-81ea-4200-4df0-940a-e9b4-ca75.ngrok.io",
 });
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -94,7 +94,6 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     resizeMode: "stretch",
-    borderColor: "blue",
   },
   buttonContainer: {
     borderWidth: 1,
