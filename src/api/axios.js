acc1d718653a1d5105082a5f02c605402b857b19
Scuftz <shivneel.singh1600@gmail.com>
2021-09-08 17:09:03 +1200

    Incorrect Sign In and Working Dental Chart

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://da5d-2407-7000-81ea-4200-4df0-940a-e9b4-ca75.ngrok.io",
+  baseURL: "http://7d8c-2407-7000-81ea-4200-9892-8413-ef03-aa60.ngrok.io",
 });
