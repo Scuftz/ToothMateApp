6c46d2bb1c33ced38377418c1b042bcc06c02b3c
SandeshPathak <sandesh.pathak07@gmail.com>
2021-09-12 15:35:57 +1200

    axios site address change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://80f0-2407-7000-a23f-5400-3c37-7f41-ab7-b42.ngrok.io",
+  baseURL: "https://f52d-2407-7000-a23f-5400-3c37-7f41-ab7-b42.ngrok.io",
 });
