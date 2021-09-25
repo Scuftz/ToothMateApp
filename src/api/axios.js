89cd71a7fe1e13a93448745006facacecdc9255a
SandeshPathak <sandesh.pathak07@gmail.com>
2021-09-26 12:43:45 +1300

    Tiny color changes on Education Screen

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://bafc15041635.ngrok.io",
+  baseURL: "https://41b5-2407-7000-a23f-5400-d0a2-d533-7131-26b5.ngrok.io",
 });
