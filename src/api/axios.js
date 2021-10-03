f94ff7bc114603b4a464b824ee7c67d7263bf5d8
SandeshPathak <sandesh.pathak07@gmail.com>
2021-10-03 13:43:20 +1300

    Sign up Screen Color Changes and removed Placeholder

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://f8c11ccd0d54.ngrok.io",
+  baseURL: "https://bbee-2407-7000-a23f-5400-d0b0-860e-ee2d-5fe5.ngrok.io",
 });
