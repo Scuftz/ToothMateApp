44882bd19a12a8137901519164b93359eb5f71e5
SandeshPathak <sandesh.pathak07@gmail.com>
2021-10-09 15:23:32 +1300

    Password Change Screen CSS Changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://d93d8008c77c.ngrok.io",
+  baseURL: "https://dcbb-2407-7000-a23f-5400-1021-ae3e-1a85-176f.ngrok.io",
 });
