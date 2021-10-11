d4397dd4d24665e225b85e4ac2e5f36bce995206
SandeshPathak <sandesh.pathak07@gmail.com>
2021-10-11 18:23:57 +1300

    Axios change only

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://dcbb-2407-7000-a23f-5400-1021-ae3e-1a85-176f.ngrok.io",
+  baseURL: "https://dd08-2407-7000-a23f-5400-b99f-a4-e454-69d4.ngrok.io",
 });
