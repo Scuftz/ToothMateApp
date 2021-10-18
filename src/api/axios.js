a5d51838f1718b0a77d8368e3cd0b39e6ac06360
SandeshPathak <sandesh.pathak07@gmail.com>
2021-10-18 21:17:48 +1300

    Changes to the UserScreen

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://765c8fd989cf.ngrok.io",
+  baseURL: "https://7fa3-2407-7000-a23f-5400-f83a-dcb0-482a-c82e.ngrok.io",
 });
