67a5732a740de3976347e600bd815e4ef4905293
Jona Stevenson <jonatutorials@gmail.com>
2022-08-11 20:58:11 +1200

    Temp mobile viewport fix
    - Fixes the issue of the address bars on iOS Safari and Chrome covering the helper text due to viewport issues.

--- react-dental-chart/src/App.js
+++ react-dental-chart/src/App.js
@@ -1,10 +1,14 @@
-import WholeMouth from './WholeMouth';
+import WholeMouth from './components/WholeMouth';
 
 export default function App() {
   return (
     <div className='container'>
       <WholeMouth />
-      <div style={{ marginBottom: 20 }}>
+      <div
+        style={{
+          marginBottom: 105 /* temp fix until I figure out a way to accurately represent 100vh on mobile */,
+        }}
+      >
         <p>
           Tap and drag to interact with the mouth. Tap a tooth to view further
           details.
