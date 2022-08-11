06a4641bf1a91c25b1a52d74b1e25f203a1394f3
Jona Stevenson <jonatutorials@gmail.com>
2022-08-11 19:43:53 +1200

    Initialise React Project

---
+++ react-dental-chart/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './styles.css';
+import WholeMouth from './WholeMouth';
+
+ReactDOM.render(<WholeMouth />, document.getElementById('root'));
