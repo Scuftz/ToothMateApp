b293bb9fd6a52af59dc658358723cf1afa866a51
supercoolcharles <80949791+supercoolcharles@users.noreply.github.com>
2022-09-18 14:05:50 +1200

    update:removed package and package-lock

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import {useRef } from "react";
+import {FaBars,Fatimes} from "react-icons/fa";
+
+function Navbar(){
+    return(
+        <header>
+            <h3>logo</h3>
+            <nav>
+                <a href="/#">Home</a>
+                <a href="/#">Login</a>
+                <button>
+                    
+                </button>
+            </nav>
+        </header>
+    )
+
+}
+export default Navbar;
