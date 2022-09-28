9c7cb0d5af3a2f87a349f726d19f93498b9aaedc
catwrightb <catharine.baker@colorado.edu>
2022-09-28 21:38:17 +1300

    fix conflict

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://0c90-2406-e003-11d8-5e01-85ce-63ef-4f07-929f.au.ngrok.io',
+  baseURL: 'https://124a-103-8-140-101.au.ngrok.io',
 });
